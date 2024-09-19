aefcc24284bdba3e7d4c0330c8a2c4a86971cf48
Daly-mail <floplid1@gmail.com>
2024-09-19 10:21:29 -0400

    hi

--- Class5.hs
+++ Class5.hs
@@ -7,4 +7,13 @@ minAndMax x y
   | x < y   = (x,y)
   | otherwise = (y,x)
 
-  
+--ListComprehension slide 13
+addOrdPairs :: [(Int,Int)] -> [Int]
+addOrdPairs pairs = [ x+y | (x,y) <- pairs, x <= y]
+--returns the sum of pairs whose first term(x) is <= the second term (y)
+
+--ListComprehension Slide 14
+allEven :: [Int] -> Bool
+allEven xs = [True | x <- xs] == [x `mod` 2 == 0 | x <- xs]
+--takes a list of int's and returns whether every item is even
+
