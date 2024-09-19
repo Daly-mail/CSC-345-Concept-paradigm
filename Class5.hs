0cae6ab898065a1b29fdaf678ab51e728e35504a
Daly-mail <floplid1@gmail.com>
2024-09-19 19:09:46 -0400

    hi

--- Class5.hs
+++ Class5.hs
@@ -14,6 +14,18 @@ addOrdPairs pairs = [ x+y | (x,y) <- pairs, x <= y]
 
 --ListComprehension Slide 14
 allEven :: [Int] -> Bool
-allEven xs = [True | x <- xs] == [x `mod` 2 == 0 | x <- xs]
+--allEven xs = [True | x <- xs] == [x `mod` 2 == 0 | x <- xs]
+allEven xs = xs == [x | x <- xs, x `mod` 2 == 0]
 --takes a list of int's and returns whether every item is even
 
+--ListComprehension Slide 15
+divisors :: Int -> [Int]
+divisors n = [ x | x <- [1..n], n `mod` x == 0]
+--checks all numbers in [1..n] to see if n can evenly be divided into x for prime number-age
+
+isPrime :: Int -> Bool
+isPrime n = [1,n] == divisors n
+--checks list of 1 and n equals to divisors's function of inputted n
+
+find :: Char -> [(Char,Int)] -> [Int]
+find key table = [ v | (k,v) <- table, key == k]
